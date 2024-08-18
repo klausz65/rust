a3296152ab13640306146c4a7652d5a692866231
Ben Kimock <kimockb@gmail.com>
2024-08-17 21:44:56 -0400

    Use more CGUs in incremental compilation

--- compiler/rustc_middle/src/mir/mono.rs
+++ compiler/rustc_middle/src/mir/mono.rs
@@ -118,15 +118,7 @@ impl<'tcx> MonoItem<'tcx> {
                     return InstantiationMode::LocalCopy;
                 }
 
-                // Finally, if this is `#[inline(always)]` we're sure to respect
-                // that with an inline copy per CGU, but otherwise we'll be
-                // creating one copy of this `#[inline]` function which may
-                // conflict with upstream crates as it could be an exported
-                // symbol.
-                match tcx.codegen_fn_attrs(instance.def_id()).inline {
-                    InlineAttr::Always => InstantiationMode::LocalCopy,
-                    _ => InstantiationMode::GloballyShared { may_conflict: true },
-                }
+                InstantiationMode::GloballyShared { may_conflict: true }
             }
             MonoItem::Static(..) | MonoItem::GlobalAsm(..) => {
                 InstantiationMode::GloballyShared { may_conflict: false }
