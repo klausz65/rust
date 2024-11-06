3841a4e7d24957d27d4df292c6198f56b6c56847
Michael Goulet <michael@errs.io>
2024-11-06 17:53:59 +0000

    Stabilize async closures

--- tests/ui/async-await/async-closures/inline-body.rs
+++ tests/ui/async-await/async-closures/inline-body.rs
@@ -6,7 +6,7 @@
 // `mir_inliner_callees` for the synthetic by-move coroutine body since
 // its def-id wasn't previously being considered.
 
-#![feature(async_closure, noop_waker)]
+#![feature(noop_waker)]
 
 use std::future::Future;
 use std::pin::pin;
