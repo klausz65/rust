6cd9e62ecc5f02359bedfb2718b8378f0693fe0a
Ralf Jung <post@ralfj.de>
2024-11-24 12:35:50 +0100

    reject aarch64 target feature toggling that would change the float ABI

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -2604,6 +2604,18 @@ impl TargetOptions {
             }
         })
     }
+
+    pub(crate) fn has_neg_feature(&self, search_feature: &str) -> bool {
+        self.features.split(',').any(|f| {
+            if let Some(f) = f.strip_prefix('-')
+                && f == search_feature
+            {
+                true
+            } else {
+                false
+            }
+        })
+    }
 }
 
 impl Default for TargetOptions {
