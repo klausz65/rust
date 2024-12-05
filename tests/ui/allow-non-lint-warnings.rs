ee9e8d296346965e4231731c27e2d1a97995fadc
Jieyou Xu <jieyouxu@outlook.com>
2024-12-05 08:09:02 +0000

    Document `allow-non-lint-warnings.rs`

--- tests/ui/allow-non-lint-warnings.rs
+++ tests/ui/allow-non-lint-warnings.rs
@@ -1,3 +1,15 @@
+// ignore-tidy-linelength
+//! Check that `-A warnings` cli flag applies to *all* warnings, including feature gate warnings.
+//!
+//! This test tries to exercise that by checking that the "empty trait list for derive" warning for
+//! `#[derive()]` is permitted by `-A warnings`, which is a non-lint warning.
+//!
+//! # Relevant context
+//!
+//! - Original impl PR: <https://github.com/rust-lang/rust/pull/21248>.
+//! - RFC 507 "Release channels":
+//!   <https://github.com/rust-lang/rfcs/blob/c017755b9bfa0421570d92ba38082302e0f3ad4f/text/0507-release-channels.md>.
+
 //@ compile-flags: -Awarnings
 //@ check-pass
 
